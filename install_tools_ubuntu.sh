d1a8390807d492ab65778ae1d9580534d6470a9b
Cheng Long <chenglong@buuuk.com>
2013-07-22 10:46:54 +0800

    Added install_tools_ubuntu.sh

---
+++ install_tools_ubuntu.sh
@@ -0,0 +1,17 @@
+# install git
+sudo apt-get update && sudo apt-get install git
+
+# install zsh
+sudo apt-get install zsh
+
+# install oh my zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# Make ZSH default
+chsh -s $(which zsh)
+
+# install rvm
+\curl -L https://get.rvm.io | bash
+
+# install ruby 2.0.0
+rvm install 2.0.0
