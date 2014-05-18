b7e1d39734db7e0a13daafb242e57095e99889c2
Cheng Long <jerome.long.cheng@gmail.com>
2014-05-18 13:43:07 +0800

    added htop and ccze

--- install_tools_ubuntu.sh
+++ install_tools_ubuntu.sh
@@ -13,5 +13,11 @@ sudo apt-get install -y zsh
 # install oh my zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
+# install htop
+sudo apt-get install -y htop
+
+# install ccze
+sudo apt-get install -y ccze
+
 # Make ZSH default
 chsh -s $(which zsh)
