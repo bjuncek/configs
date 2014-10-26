6d7677aa2e88ee1ce88ed9aedb6c9d3840452d4a
Cheng Long <jerome.long.cheng@gmail.com>
2014-10-26 15:18:49 +0800

    Update ruby installation

--- install_tools_ubuntu.sh
+++ install_tools_ubuntu.sh
@@ -2,7 +2,9 @@
 sudo apt-get install -y python-software-properties
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
+sudo apt-get install -y build-essential
 sudo apt-get install -y ruby2.1
+sudo apt-get install -y ruby2.1-dev
 
 # install git
 sudo apt-get install -y git
