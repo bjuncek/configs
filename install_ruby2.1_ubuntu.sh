df5c9ca9c33f167a4a5f30d6ba3b600a50168ed9
Cheng Long <jerome.long.cheng@gmail.com>
2014-10-28 00:34:10 +0800

    extract out ruby 2.1 installation

---
+++ install_ruby2.1_ubuntu.sh
@@ -0,0 +1,8 @@
+# install ruby 2.1
+sudo apt-get install -y python-software-properties
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install -y build-essential
+sudo apt-get install -y ruby2.1
+sudo apt-get install -y ruby2.1-dev
+sudo apt-get install -y libsqlite3-dev
--- install_tools_ubuntu.sh
+++ install_tools_ubuntu.sh
@@ -1,11 +1,4 @@
-# install ruby 2.1
-sudo apt-get install -y python-software-properties
-sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo apt-get install -y build-essential
-sudo apt-get install -y ruby2.1
-sudo apt-get install -y ruby2.1-dev
-sudo apt-get install -y libsqlite3-dev
 
 # install git
 sudo apt-get install -y git
