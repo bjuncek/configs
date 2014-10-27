5804003a1c2b5a924734c006fd6f732837cd67aa
Cheng Long <jerome.long.cheng@gmail.com>
2014-10-28 00:38:47 +0800

    assume yes for apt-add-repository

--- install_ruby2.1_ubuntu.sh
+++ install_ruby2.1_ubuntu.sh
@@ -1,6 +1,6 @@
 # install ruby 2.1
 sudo apt-get install -y python-software-properties
-sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install -y build-essential
 sudo apt-get install -y ruby2.1
