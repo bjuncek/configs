616a3eb78689d6845bf0edcbcec14407f537f021
Bruno Korbar <bjuncek@gmail.com>
2020-01-15 12:15:18 +0000

    debugging

--- setup.sh
+++ setup.sh
@@ -138,7 +138,7 @@ else
 fi
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # copy .vimrc from my github
 curl -LSso ~/.vimrc https://raw.github.com/bjuncek/configs/master/.vimrc
